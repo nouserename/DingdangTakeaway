c6ae98b1c9a4f5e64f7f8c0d195b874785991c6e
nouserename <48287164+nouserename@users.noreply.github.com>
2020-04-10 19:25:43 +0800

    finish experiment 2

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
     <div>
-        <!--<transition name="router-fade" mode="out-in"> 为路由切换设置过渡效果  -->
         <transition name="router-fade" mode="out-in">
-            <router-view></router-view>
+                <router-view ></router-view>
         </transition>
+        <!-- HTML对大小写不敏感，所以驼峰命名的组件名字，要改为短划线命名 -->
+        <svg-icon></svg-icon>    
     </div>
 </template>
 
 <script>
+    import svgIcon from './components/common/svgIcon';
+      export default {
+          //必须注册才能使用
+        components:{
+            svgIcon
+        },
+      }
 
 </script>
 
 <style lang="scss">
-    /** 使用@import 将前面写的公共样式引入，清除默认样式**/
       @import './style/common';
-    /** 路由切换过渡效果 透明过渡 **/
     .router-fade-enter-active, .router-fade-leave-active {
           transition: opacity .3s;
     }
