a45b68ae571d5d1af2fd51b1266216686c86b98a
nouserename <48287164+nouserename@users.noreply.github.com>
2020-04-10 16:18:23 +0800

    edit index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'  //@ 会根据配置自动 映射成src 文件夹
+import HelloWorld from '@/components/HelloWorld'  //@ 会根据配置自动 转换成src 文件夹
 import Home from '@/components/Home'
+import App from '../App' //引入 App.vue
 
-Vue.use(Router)
+Vue.use(Router) //在vue中注册 vue-router
 
 export default new Router({
+  mode:'hash',//指定路由为 hash 模式
   routes: [
     {
-      path: '/', 
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path:'/home',//路由路径
-      name:'Home',
-      component:Home //路由指定渲染那个组件
+      path: '/',
+      component: App,
+      children:[ //二级路由
+      {
+          path:'',//路由为根路由自动重定向到 /home
+          redirect:'/home'
+      },
+        {
+        path:'/home',
+        component:Home
+      },
+      {
+        path:'/helloworld',
+        component:HelloWorld
+      }
+      ]
     }
   ]
 })
