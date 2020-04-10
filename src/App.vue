4ad706fa5957f7a55bcd1a98bdb9aab3259ce97f
nouserename <48287164+nouserename@users.noreply.github.com>
2020-04-10 16:21:05 +0800

    edit App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div>
+        <!--<transition name="router-fade" mode="out-in"> 为路由切换设置过渡效果  -->
+        <transition name="router-fade" mode="out-in">
+            <router-view></router-view>
+        </transition>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+    /** 使用@import 将前面写的公共样式引入，清除默认样式**/
+      @import './style/common';
+    /** 路由切换过渡效果 透明过渡 **/
+    .router-fade-enter-active, .router-fade-leave-active {
+          transition: opacity .3s;
+    }
+    .router-fade-enter, .router-fade-leave-active {
+          opacity: 0;
+    }
 </style>
