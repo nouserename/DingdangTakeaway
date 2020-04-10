683118100f77755eda1e4d5282375571c3dbf21d
nouserename <48287164+nouserename@users.noreply.github.com>
2020-04-10 11:45:58 +0800

    before install vuex

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Home page</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Home'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/HelloWorld'  //@ 会根据配置自动 映射成src 文件夹
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/', 
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path:'/home',//路由路径
+      name:'Home',
+      component:Home //路由指定渲染那个组件
     }
   ]
 })
